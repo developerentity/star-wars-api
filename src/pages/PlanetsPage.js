f93e9a0ce7c0f740b9a7846b13974dbf78f6fbd4
just13just <bkryzhniy@gmail.com>
2020-09-08 19:41:09 +0300

    fix: delete bug not iterable property of data array

--- src/pages/PlanetsPage.js
+++ src/pages/PlanetsPage.js
@@ -4,31 +4,20 @@ import PlanetsItem from './../elements/PlanetsItems';
 
 const PlanetsPage = () => {
 
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
     const [btn, setBtn] = useState(false);
 
     useEffect(() => {
 
         fetch(`https://swapi.dev/api/planets/`)
             .then(res => res.json())
-            .then(res => setData(res))
+            .then(res => setData(res.results))
             .catch(err => console.error(err))
 
     }, [btn])
 
-
-    console.log(data.results)
-
-    // let planetsArr = [...data.results]
-
-    let pseudoPlanetsArr = [
-        { key: 1, name: "Tatooine" },
-        { key: 2, name: "Alderaan" },
-        { key: 3, name: "Yavin IV" },
-        { key: 4, name: "Hoth" }]
-
-    let planetsElements = pseudoPlanetsArr
-        .map(planet => <PlanetsItem key={planet.key} name={planet.name} />)
+    let planetsElements = data
+        .map(planet => <PlanetsItem key={planet.name} name={planet.name} />)
 
     return (
         <div className="content-wrap planets">
