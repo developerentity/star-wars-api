3ec64b68ff9dab6b95a4ead2d01957d1d94020e2
just13just <bkryzhniy@gmail.com>
2020-09-10 19:19:32 +0300

    refactor: rebuild clock component with hooks

--- src/pages/ClockPage.js
+++ src/pages/ClockPage.js
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './../App.css';
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-    }
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { date: new Date() };
+//     }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
+//     componentDidMount() {
+//         this.timerID = setInterval(
+//             () => this.tick(),
+//             1000
+//         );
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timerID)
+//     }
+
+//     tick() {
+//         this.setState({
+//             date: new Date()
+//         });
+//     }
+
+//     render() {
+// return (
+//     <div>
+//         <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+//     </div>
+// );
+//     }
+// }
+
+// export default Clock;
+
+const Clock = () => {
+
+    const [time, setTime] = useState(`${new Date()}`)
+    
+    useEffect(() => {
+        setInterval(
+            () => tick(),
             1000
-        );
+        )
+    }, [])
+
+    const tick = () => {
+        setTime(`${new Date()}`)
     }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID)
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h2>Сейчас {time}</h2>
+        </div>
+    );
 }
 
 export default Clock;
