b1ef418602d3939dbdb93db5559a34b1a63fc19b
just13just <bkryzhniy@gmail.com>
2020-09-10 14:06:55 +0300

    refactor: change the clock view

--- src/pages/ClockPage.js
+++ src/pages/ClockPage.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './../App.css';
 
-function tick() {
-    const element = (
+function Clock(props) {
+    return (
         <div>
-            <h2>Сейчас {new Date().toLocaleTimeString()}.</h2>
+            <h2>Сейчас {props.date.toLocaleTimeString()}.</h2>
         </div>
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
     );
 }
 
+function tick() {
+        ReactDOM.render(
+            <Clock date={new Date()} />,
+            document.getElementById('root')
+        );
+    }
 setInterval(tick, 1000);
 
-export default tick;
+export default Clock;
